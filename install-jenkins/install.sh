3ba16d9768ea305007ff9f561811da29fbb8fcbd
craldo <crizgiraldo@gmail.com>
2021-04-25 15:49:59 -0500

    pruebas con docker build

--- install-jenkins/install.sh
+++ install-jenkins/install.sh
@@ -1,9 +1,10 @@
 # Depending on your system you might need to use sudo
-apt-get update
-apt-get install openjdk-8-jdk wget gnupg
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get -y update
+sudo apt-get -y install openjdk-8-jdk wget gnupg
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
-apt-get update
-apt-get install git jenkins
-ssh-keygen
-service jenkins start
+sudo apt-get update
+sudo apt-get install git jenkins
+sudo ssh-keygen
+sudo service jenkins start
